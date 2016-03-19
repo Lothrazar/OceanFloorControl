64d5f0faac49dc8dfa90a4d6770c0864af16d290
sam bassett <samson.bassett@gmail.com>
2016-03-19 09:40:06 -0700

    update to 1.9

--- src/main/java/com/lothrazar/samsocean/ConfigOcean.java
+++ src/main/java/com/lothrazar/samsocean/ConfigOcean.java
@@ -2,31 +2,33 @@ package com.lothrazar.samsocean;
 
 import net.minecraftforge.common.config.Configuration;
 
-public class ConfigOcean
-{ 
+public class ConfigOcean{
+
 	private Configuration instance;
 	private String category = ModOcean.MODID;
 
-	public Configuration instance()
-	{
+	public Configuration instance(){
+
 		return instance;
 	}
-	
-	public ConfigOcean(Configuration c)
-	{
-		instance = c; 
+
+	public ConfigOcean(Configuration c){
+
+		instance = c;
 		instance.load();
 
-		clayNumBlocks = instance.get(category,"clay_size",32).getInt();
-		clayChance = instance.get(category,"clay_chance",65).getInt();
-		
-		sandNumBlocks = instance.get(category,"sand_size",22).getInt();
-		sandChance = instance.get(category,"sand_chance",45).getInt();
+		clayNumBlocks = instance.get(category, "clay_size", 32).getInt();
+		clayChance = instance.get(category, "clay_chance", 65).getInt();
 
-		dirtNumBlocks = instance.get(category,"dirt_size",18).getInt();
-		dirtChance = instance.get(category,"dirt_chance",30).getInt();
+		sandNumBlocks = instance.get(category, "sand_size", 22).getInt();
+		sandChance = instance.get(category, "sand_chance", 45).getInt();
 
-		if(instance.hasChanged()){ instance.save(); }
+		dirtNumBlocks = instance.get(category, "dirt_size", 18).getInt();
+		dirtChance = instance.get(category, "dirt_chance", 30).getInt();
+
+		if(instance.hasChanged()){
+			instance.save();
+		}
 	}
 
 	public int clayChance;
--- src/main/java/com/lothrazar/samsocean/ModOcean.java
+++ src/main/java/com/lothrazar/samsocean/ModOcean.java
@@ -1,4 +1,5 @@
 package com.lothrazar.samsocean;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
@@ -7,31 +8,31 @@ import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-  
-@Mod(modid = ModOcean.MODID, useMetadata = true ,updateJSON="https://raw.githubusercontent.com/LothrazarMinecraftMods/OceanFloorControl/master/update.json")  
-public class ModOcean
-{	
+
+@Mod(modid = ModOcean.MODID, useMetadata = true, updateJSON = "https://raw.githubusercontent.com/LothrazarMinecraftMods/OceanFloorControl/master/update.json")
+public class ModOcean{
+
 	public static final String MODID = "samsocean";
 	int weight = 0;
 
 	@Instance(value = MODID)
-	public static ModOcean instance;	
+	public static ModOcean instance;
 
 	public static ConfigOcean cfg;
 
 	@EventHandler
-	public void onPreInit(FMLPreInitializationEvent event)
-	{ 
+	public void onPreInit(FMLPreInitializationEvent event){
+
 		cfg = new ConfigOcean(new Configuration(event.getSuggestedConfigurationFile()));
-	  
-		MinecraftForge.EVENT_BUS.register(instance); 
+
+		MinecraftForge.EVENT_BUS.register(instance);
 		MinecraftForge.TERRAIN_GEN_BUS.register(instance);
-		MinecraftForge.ORE_GEN_BUS.register(instance); 
+		MinecraftForge.ORE_GEN_BUS.register(instance);
 	}
-	
+
 	@EventHandler
-	public void onInit(FMLInitializationEvent event)
-	{       
+	public void onInit(FMLInitializationEvent event){
+
 		GameRegistry.registerWorldGenerator(new WorldGeneratorOcean(), weight);
 	}
 }
