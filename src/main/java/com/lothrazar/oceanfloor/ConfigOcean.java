8eaa4727f8668c487a59b1948bed4ec9a2074e64
lothrazar <samson.bassett@gmail.com>
2023-07-04 08:34:47 -0700

    1.20.1 ported

--- src/main/java/com/lothrazar/oceanfloor/ConfigOcean.java
+++ src/main/java/com/lothrazar/oceanfloor/ConfigOcean.java
@@ -1,44 +1,20 @@
 package com.lothrazar.oceanfloor;
 
-import java.nio.file.Path;
-import com.electronwill.nightconfig.core.file.CommentedFileConfig;
-import com.electronwill.nightconfig.core.io.WritingMode;
 import com.lothrazar.library.config.ConfigTemplate;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.common.ForgeConfigSpec.IntValue;
-import net.minecraftforge.fml.loading.FMLPaths;
 
 public class ConfigOcean extends ConfigTemplate {
 
   private static final ForgeConfigSpec.Builder CFG = new ForgeConfigSpec.Builder();
-  private static ForgeConfigSpec COMMON_CONFIG;
-  public static IntValue DIRTSIZE;
-  public static IntValue DIRTSPREAD;
-  public static IntValue CLAYSPREAD;
-  public static IntValue CLAYSIZE;
-  public static IntValue SANDSIZE;
-  public static IntValue SANDSPREAD;
-  public static IntValue GRAVELSIZE;
-  public static IntValue GRAVELSPREAD;
+  private static ForgeConfigSpec CONFIG;
   public static final String WALL = "####################################################################################";
   static {
-    initConfig();
-  }
-
-  public static void setup() {
-    Path path = FMLPaths.CONFIGDIR.get().resolve(ModOcean.MODID + ".toml");
-    final CommentedFileConfig configData = CommentedFileConfig.builder(path)
-        .sync()
-        .autosave()
-        .writingMode(WritingMode.REPLACE)
-        .build();
-    configData.load();
-    COMMON_CONFIG.setConfig(configData);
-  }
-
-  private static void initConfig() {
-    CFG.comment(WALL, "Features moved to the data pack", WALL).push(ModOcean.MODID);
+    CFG.comment(WALL, "Configured Features moved to the data pack; see the /oceanfloor/worldgen/ folder", WALL).push(ModOcean.MODID);
     CFG.pop(); //ROOT
-    COMMON_CONFIG = CFG.build();
+    CONFIG = CFG.build();
+  }
+
+  public ConfigOcean() {
+    CONFIG.setConfig(setup(ModOcean.MODID));
   }
 }
--- src/main/java/com/lothrazar/oceanfloor/ModOcean.java
+++ src/main/java/com/lothrazar/oceanfloor/ModOcean.java
@@ -1,9 +1,6 @@
 package com.lothrazar.oceanfloor;
 
-import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ModOcean.MODID)
 public class ModOcean {
@@ -16,14 +13,6 @@ public class ModOcean {
    * datapack /worldgen/ folder
    */
   public ModOcean() {
-    ConfigOcean.setup();
-    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-    GenerationStep.Decoration.UNDERGROUND_ORES.getName();
-  }
-
-  private void setup(final FMLCommonSetupEvent event) {
-    event.enqueueWork(() -> {
-      //      WorldGenRegistry.init();
-    });
+    new ConfigOcean();
   }
 }
